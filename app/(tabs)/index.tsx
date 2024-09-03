73504ac710d0158c7992d81ebd73591ed99c7592
basdidon <39435008+basdidon@users.noreply.github.com>
2024-09-03 19:44:25 +0700

    query data from server side with tanstack

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import { useAppSelector, useAppDispatch } from "../hooks";
 import { increment, decrement } from "@/features/counter/counterSlice";
-import { IconButton, MD2Colors as Colors } from "react-native-paper";
+import { IconButton, MD2Colors as Colors, Icon, MD2Colors } from "react-native-paper";
+import { useQuery } from "@tanstack/react-query";
+
+const baseUrl = "http://192.168.1.23:5000";
 
 export default function Index() {
     const count = useAppSelector((state) => state.counter.value);
     const dispatch = useAppDispatch();
+
+    const { isPending, data, isError } = useQuery({
+        queryKey: ["repoData"],
+        queryFn: () => fetch(baseUrl).then((res) => res.text()),
+    });
+
+    const productQuery = useQuery({
+        queryKey: ["product/"],
+        queryFn: () => fetch(baseUrl + "/api/products/8857127442037").then((res) => res.json()),
+    });
+
     return (
         <View
             style={{
@@ -43,6 +57,17 @@ export default function Index() {
             >
                 View user details
             </Link>
+            <View style={{ flexDirection: "row" }}>
+                {isPending ? (
+                    <ActivityIndicator animating={isPending} color={MD2Colors.red800} />
+                ) : (
+                    <Icon source={isError ? "error" : "check"} size={24} />
+                )}
+                <Text style={{ marginStart: 8 }}>http profile : {data ?? "loading"}</Text>
+            </View>
+            <Text>{productQuery.data.barcode}</Text>
+            <Text>{productQuery.data.name}</Text>
+            <Text>{productQuery.data.price}</Text>
         </View>
     );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,19 @@
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
     return (
         <Provider store={store}>
-            <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
+            <QueryClientProvider client={queryClient}>
+                <Stack screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                </Stack>
+            </QueryClientProvider>
         </Provider>
     );
 }
